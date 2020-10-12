b249cfbe392b6f637bf0d51f602d7e67cce3a7d2
Andres Leal <pixele9@gmail.com>
2020-10-12 16:21:19 -0500

    *added* new routes and added nodemon

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
+
+
+let pokes = [] 
+pokemon.forEach(el => pokes.push(el.name))
 
 app.get("/", (req, res, next) => {
     res.status(200)
-    res.send("Hello World!!!!")
+    res.send("Welcome to the server")
 })
 
-app.listen(3000, () => console.log("server is running..."));
+app.get("/pokemon/names", (req, res, next) => {
+    res.status(200)
+    res.send(pokes)
+})
+
+app.get("/pokemon/all", (req, res, next) => {
+    res.status(200)
+    res.send(pokemon)
+})
+
+app.get('/pokemon/:id', (req, res, next) => {
+    let pokeID = req.params.id-1
+    if (pokeID < pokes.length && pokeID > 0) {
+        res.status(200)
+        res.send(pokemon[pokeID])
+    } else {
+        res.status(404)
+        res.send("<h1>Pokemond ID not existent</h1>")
+    }
+})
+
+app.listen(process.env.port || 3000, () => console.log("server is running..."));
