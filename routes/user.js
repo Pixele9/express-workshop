5d046e2e48b5af88db882e32a2643311ffec74a4
Andres Leal <pixele9@gmail.com>
2020-10-29 20:38:09 -0600

    *added* user endpoint

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const user = express.Router();
+const db = require("../config/database"); 
+
+user.post("/", async (req, res, next) => {
+    const { user_name, user_mail, user_password } = req.body; 
+
+    let query = "insert into user (user_name, user_mail, user_password) "
+    query += `values('${user_name}', '${user_mail}', '${user_password}')`
+
+    const rows = await db.query(query)
+
+    if(rows.affectedRows) {
+        return res.status(200).json({code:200, message: `User ${user_name} successfuly signed up`})
+    }
+
+    return res.status(500).json({code:500, message: "Something went wrong"})
+})
