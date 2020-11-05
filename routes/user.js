9acad23239f8c8b12508296eb8322b00e1e78357
Andres Leal <pixele9@gmail.com>
2020-11-05 00:00:30 -0600

    *added* user stuff

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,19 @@ const db = require("../config/database");
 user.post("/", async (req, res, next) => {
     const { user_name, user_mail, user_password } = req.body; 
 
-    let query = "insert into user (user_name, user_mail, user_password) "
-    query += `values('${user_name}', '${user_mail}', '${user_password}')`
+    if (user_name, user_mail, user_password ) {
+        let query = "insert into user (user_name, user_mail, user_password) "
+        query += `values('${user_name}', '${user_mail}', '${user_password}')`
 
-    const rows = await db.query(query)
+        const rows = await db.query(query)
 
-    if(rows.affectedRows) {
-        return res.status(200).json({code:200, message: `User ${user_name} successfuly signed up`})
+        if(rows.affectedRows) {
+            return res.status(200).json({code:200, message: `User ${user_name} successfuly signed up`})
+        }
+
+        return res.status(500).json({code:500, message: "Something went wrong"})
     }
-
-    return res.status(500).json({code:500, message: "Something went wrong"})
+    return res.status(500).json({code:500, message: "Incomplete fields"})
 })
+
+module.exports = user
